43dcd77ced93c380c460b0f18ba4355cfe3621cd
Akrity Priya <akritipriya1905@gamil.com>
2024-12-24 00:01:03 +0530

    feat:backend signup and login

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute=require("./routes/user");
+dotenv.config();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "try.html"));
+});
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api/user", userRoute);
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000");
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("MongoDB connected");
+    }).catch((err) => {
+        console.log(err);
+    });
+});
